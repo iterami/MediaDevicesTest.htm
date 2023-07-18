401f3e4748764bc1c6b13a981c1714586513db68
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 13:51:42 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,27 @@
 'use strict';
 
+function audio_reset(){
+    if(audio_stream !== 0){
+        const tracks = audio_stream.getTracks();
+        tracks.forEach(function(track){
+            track.stop();
+        });
+        audio_stream = 0;
+    }
+
+    if(audio_node !== false){
+        audio_node.disconnect();
+        audio_node.onaudioprocess = void 0;
+        audio_node = false;
+    }
+
+    const audio_volume_element = document.getElementById('audio-volume-range');
+    audio_volume_element.classList.add('hidden');
+    audio_volume_element.value = 0;
+
+    document.getElementById('results-audio').textContent = '';
+}
+
 function repo_escape(){
     if(!core_menu_open){
         audio_reset();
@@ -106,4 +128,23 @@ function repo_init(){
       'menu': true,
       'title': 'MediaDevicesTest.htm',
     });
+}
+
+function video_reset(){
+    if(video_stream !== 0){
+        const tracks = video_stream.getTracks();
+        tracks.forEach(function(track){
+            track.stop();
+        });
+        video_stream = 0;
+    }
+
+    const video_element = document.getElementById('video-element');
+    video_element.classList.add('hidden');
+    video_element.pause();
+    video_element.removeAttribute('srcObject');
+    video_element.removeAttribute('src');
+    video_element.load();
+
+    document.getElementById('results-video').textContent = '';
 }
