4b7939999e98c3af16c8ca1c60749c6418fc0254
Jan Hořava <czechinthemail@gmail.com>
2026-04-16 03:59:06 -0700

    fixed ui newlines, adjusted ids, improved formatting/ordering

--- repo.js
+++ repo.js
@@ -1,142 +1,107 @@
 'use strict';
 
-function audio_output_left(){
-    audio_start('boop-left');
-}
-
-function audio_output_middle(){
-    audio_start('boop');
-}
-
-function audio_output_right(){
-    audio_start('boop-right');
-}
-
-function audio_reset(){
-    if(audio_stream !== 0){
-        const tracks = audio_stream.getTracks();
-        tracks.forEach(function(track){
-            track.stop();
-        });
-        audio_stream = 0;
-    }
-
-    if(audio_node !== false){
-        audio_node.disconnect();
-        audio_node.onaudioprocess = void 0;
-        audio_node = false;
-    }
-
-    core_elements.audio_volume.classList.add('hidden');
-    core_elements.audio_volume.value = 0;
-
-    core_elements.results_audio.textContent = '';
-}
-
 function repo_init(){
     core_repo_init({
       'beforeunload': {
         'todo': function(){
-            audio_reset();
-            video_reset();
+            reset_audio();
+            reset_video();
         }
       },
       'events': {
-        'audio_left': {
-          'onclick': audio_output_left,
+        'left_audio': {
+          'onclick': function(){
+              audio_start('boop_left');
+          },
         },
-        'audio_middle': {
-          'onclick': audio_output_middle,
+        'middle_audio': {
+          'onclick': function(){
+              audio_start('boop');
+          },
         },
-        'audio_right': {
-          'onclick': audio_output_right,
+        'reset_audio': {
+          'onclick': reset_audio,
         },
-        'audio_reset': {
-          'onclick': audio_reset,
+        'reset_video': {
+          'onclick': reset_video,
         },
-        'audio_test': {
+        'right_audio': {
+          'onclick': function(){
+              audio_start('boop_right');
+          },
+        },
+        'test_audio': {
           'onclick': function(){
               navigator.mediaDevices.getUserMedia({
                 'audio': true,
                 'video': false,
               }).then(function(stream){
-                  audio_stream = stream;
+                  stream_audio = stream;
+                  const context = new AudioContext();
 
-                  const audio_volume_element = core_elements.results_audio_volume;
-                  audio_volume_element.classList.remove('hidden');
+                  const analyser = context.createAnalyser();
+                  analyser.smoothingTimeConstant = .8;
+                  analyser.fftSize = 1024;
 
-                  core_elements.results_audio.textContent = audio_stream.id;
+                  const input = context.createMediaStreamSource(stream_audio);
+                  input.connect(analyser);
 
-                  const audio_context = new AudioContext();
-                  const audio_analyser = audio_context.createAnalyser();
+                  node = context.createScriptProcessor(2048, 1, 1);
+                  analyser.connect(node);
 
-                  audio_analyser.smoothingTimeConstant = .8;
-                  audio_analyser.fftSize = 1024;
-
-                  const input = audio_context.createMediaStreamSource(audio_stream);
-                  audio_node = audio_context.createScriptProcessor(
-                    2048,
-                    1,
-                    1
-                  );
-
-                  input.connect(audio_analyser);
-                  audio_analyser.connect(audio_node);
-
-                  audio_node.connect(audio_context.destination);
-                  audio_node.onaudioprocess = function(){
-                      const array = new Uint8Array(audio_analyser.frequencyBinCount);
-                      audio_analyser.getByteFrequencyData(array);
+                  const element = core_elements.results_audio_volume;
+                  node.connect(context.destination);
+                  node.onaudioprocess = function(){
+                      const array = new Uint8Array(analyser.frequencyBinCount);
+                      analyser.getByteFrequencyData(array);
 
                       let result = 0;
                       for(let i = 0; i < array.length; i++){
                           result += array[i];
                       }
-                      audio_volume_element.value = result / array.length;
+
+                      element.value = result / array.length;
                   };
 
-              }).catch(function(error){
-                  audio_reset();
+                  element.classList.remove('hidden');
+                  core_elements.results_audio.textContent = stream_audio.id;
 
+              }).catch(function(error){
+                  reset_audio();
                   core_elements.results_audio.textContent = error.name;
               });
           },
         },
-        'video-reset': {
-          'onclick': video_reset,
-        },
-        'video-test': {
+        'test_video': {
           'onclick': function(){
               navigator.mediaDevices.getUserMedia({
                 'audio': false,
                 'video': true,
               }).then(function(stream){
-                  video_stream = stream;
+                  stream_video = stream;
 
-                  const tracks = video_stream.getVideoTracks();
+                  const tracks = stream_video.getVideoTracks();
                   core_elements.results_video.textContent = tracks[0].label;
 
-                  core_elements.video.classList.remove('hidden');
                   core_elements.video.srcObject = stream;
+                  core_elements.video.classList.remove('hidden');
                   core_elements.video.play();
 
               }).catch(function(error){
-                  video_reset();
-
+                  reset_video();
                   core_elements.results_video.textContent = error.name;
               });
           },
         },
       },
       'globals': {
-        'audio_stream': 0,
-        'audio_node': false,
-        'video_stream': 0,
+        'node': 0,
+        'stream_audio': 0,
+        'stream_video': 0,
       },
-      'info': '<button id=audio_test type=button>Audio Input Test</button><button id=audio_reset type=button>Reset</button> <span id=results_audio></span><br>'
-        + '<progress class=hidden id=results_audio_volume max=100 min=0 value=0></progress>'
-        + 'Audio Output Test:<button id=audio_left type=button>Left</button><button id=audio_middle type=button>Middle</button><button id=audio_right type=button>Right</button><br>'
-        + '<button id=video-test type=button>Video Test</button><button id=video-reset type=button>Reset</button> <span id=results_video></span><br>'
+      'info': '<button id=test_audio type=button>Audio Input Test</button><button id=reset_audio type=button>Reset</button><progress class=hidden id=results_audio_volume max=100 min=0 value=0></progress> <span id=results_audio></span><br>'
+        + 'Audio Output Test:<button id=left_audio type=button>Left</button><button id=middle_audio type=button>Middle</button><button id=right_audio type=button>Right</button><br>'
+        + '<button id=test_video type=button>Video Test</button><button id=reset_video type=button>Reset</button> <span id=results_video></span><br>'
         + '<video class=hidden controls id=video></video>',
       'menu_lock': true,
       'title': 'MediaDevicesTest.htm',
@@ -149,12 +114,12 @@ function repo_init(){
     });
 
     audio_create({
-      'boop-left': {
+      'boop_left': {
         'panner': {
           'positionX': -1,
         },
       },
-      'boop-right': {
+      'boop_right': {
         'panner': {
           'positionX': 1,
         },
@@ -162,20 +127,39 @@ function repo_init(){
     });
 }
 
-function video_reset(){
-    if(video_stream !== 0){
-        const tracks = video_stream.getTracks();
+function reset_audio(){
+    if(stream_audio !== 0){
+        const tracks = stream_audio.getTracks();
         tracks.forEach(function(track){
             track.stop();
         });
-        video_stream = 0;
+        stream_audio = 0;
     }
 
-    core_elements.video.classList.add('hidden');
-    core_elements.video.pause();
-    core_elements.video.removeAttribute('srcObject');
-    core_elements.video.removeAttribute('src');
-    core_elements.video.load();
+    if(node !== 0){
+        node.disconnect();
+        node.onaudioprocess = void 0;
+        node = 0;
+    }
+
+    core_elements.results_audio.textContent = '';
+    core_elements.results_audio_volume.classList.add('hidden');
+}
+
+function reset_video(){
+    if(stream_video !== 0){
+        const tracks = stream_video.getTracks();
+        tracks.forEach(function(track){
+            track.stop();
+        });
+        stream_video = 0;
+    }
 
     core_elements.results_video.textContent = '';
+    const video = core_elements.video;
+    video.classList.add('hidden');
+    video.pause();
+    video.removeAttribute('srcObject');
+    video.removeAttribute('src');
+    video.load();
 }
